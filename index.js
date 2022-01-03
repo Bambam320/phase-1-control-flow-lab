954c22fb667bed493f873dd19f9439f090f58790
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-01-03 02:31:28 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+
+  if (distance < 400) { return 'This one is on me!';
+} else if (distance < 2500) {return 'I will gladly take your thirty bucks.';
+} else if (distance > 2500) {return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(location){
+  return location == 'NYC' ? ('Ok, sounds good.') : ('No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(cash){
+  switch (cash) {
+    case cash = 'generous':
+      return 'Thank you so much.';
+      break;
+      case cash = 'not as generous':
+        return 'Thank you.';
+        break;
+        default:
+          return 'Bye.';
+  }
 }
+
+
+scuberGreetingForFeet(3500);
+ternaryCheckCity(NYC);
+switchOnCharmFromTip(45);
